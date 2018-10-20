1e28beaafdae465d7124cde664a5145c49319545
Adam Forestier <aforestier10@gmail.com>
2018-10-20 15:15:23 -0500

    Print array's values in reverse

---
+++ Arrays_DS/Arrays_DS/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Arrays_DS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // test input
+            int[] arr = { 1, 4, 3, 2 };
+
+            // call method to print array in reverse
+            ReversePrint(arr);
+
+            // keep console open
+            Console.Read();
+        }
+
+        /// <summary>
+        /// Method to print array in reverse
+        /// </summary>
+        /// <param name="arr"></param>
+        private static void ReversePrint(int[] arr)
+        {
+            // initialize num of items and counter
+            int n = arr.Length - 1;
+            int i;
+
+            // print in reverse and end line after all have been printed
+            for (i = n; i >= 0; i--)
+                Console.Write(arr[i] + " ");
+            Console.WriteLine();
+
+            // function return
+            return;
+        }
+    }
+}

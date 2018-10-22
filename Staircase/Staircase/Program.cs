11dd5bf6f5b093ae8396f3887ffe99fca64c0113
Adam Forestier <aforestier10@gmail.com>
2018-10-21 19:10:52 -0500

    Print a right aligned staircase with n steps

---
+++ Staircase/Staircase/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Staircase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // input
+            int stairs = 6;
+
+            // call method to print stairs
+            PrintStairs(stairs);
+            
+            // keep console open
+            Console.Read();
+        }
+
+
+        private static void PrintStairs(int n)
+        {
+            // initialize counters
+            int i, j;
+
+            // outer loop controls row
+            for(i = 1; i <= n; i++)
+            {
+                // first inner loop controls spaces
+                for (j = n; j > i; j--)
+                    Console.Write(" ");
+
+                // second inner loop controls #
+                for (j = 1; j <= i; j++)
+                    Console.Write("#");
+
+                // move to next row
+                Console.WriteLine();
+            }
+            
+        }
+    }
+}
